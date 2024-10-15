06f8cfee3dd0d3087bdfaa4e13716bf1bf503379
InKyuInst <148037709+InKyuInst@users.noreply.github.com>
2024-10-15 09:47:47 +0900

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="h-screen flex flex-col bg-gray-800 text-white p-8" id="contact">
+    <footer className="min-h-screen flex flex-col bg-gray-800 text-white p-8 justify-between" id="contact">
         <div className="mx-8 grid grid-cols-1 lg:grid-cols-2">
             <div className="mb-8 mr-16">
                 <h2 className="text-3xl font-bold mb-4">About Me</h2>
@@ -10,52 +10,43 @@ const Footer = () => {
             </div>
             <div className="mb-8">
                 <h2 className="text-3xl font-bold mb-4">Projects & Contributions</h2>
-                <ul className="flex flex-row flex-wrap list-none justify-between divide-x divide-solid">
-                    <li className="w-1/4 px-5 flex flex-col h-96 justify-between">
+                <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none">
+                    <li className="py-5 md:mr-5 lg:mr-10">
                         <div>
                             <span className="text-2xl">
                                 Project 1
                             </span>
-                            <p>
+                            <p className="mt-3">
                                 Led the direction and visual concept, resulting in a 20% increase in audience engagement.
                             </p>
                         </div>
-                        <div>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur provident officiis neque reiciendis autem atque ab nemo! Totam officia temporibus distinctio eius! Modi corporis illo porro facere reiciendis! Et, nemo!
-                        </div>
                     </li>
-                    <li className="w-1/4 px-5 flex flex-col h-96 justify-between">
+                    <li className="py-5 lg:mr-10">
                     <div>
                             <span className="text-2xl">
                                 Project 2
                             </span>
-                            <p>
+                            <p className="mt-3">
                                 Managed post-production, ensuring seamless transitions and high-quality sound design.
                             </p>
                         </div>
-                        <div>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur provident officiis neque reiciendis autem atque ab nemo! Totam officia temporibus distinctio eius! Modi corporis illo porro facere reiciendis! Et, nemo!
-                        </div>
                     </li>
-                    <li className="w-1/4 px-5 flex flex-col h-96 justify-between">
+                    <li className="py-5 md:mr-5 lg:mr-10">
                     <div>
                             <span className="text-2xl">
                                 Project 3
                             </span>
-                            <p>
+                            <p className="mt-3">
                                 Directed a team of cinematographers, creating stunning visuals that enhanced the narrative.
                             </p>
-                        </div>
-                        <div>
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur provident officiis neque reiciendis autem atque ab nemo! Totam officia temporibus distinctio eius! Modi corporis illo porro facere reiciendis! Et, nemo!
                         </div>
                     </li>
                 </ul>
             </div>
         </div>
-        {/* <div className="flex flex-col items-center">
-            <p>&copy; 2024 Your Name</p>
-            <div className="flex space-x-4 mt-4">
+        <div className="flex flex-col items-center">
+            <h2 className="text-2xl font-bold">CONTACT</h2>
+            <div className="flex space-x-4 mt-2 mb-8">
                 <a
                     href="https://github.com/your-profile"
                     target="_blank"
@@ -81,7 +72,8 @@ const Footer = () => {
                     Instagram
                 </a>
             </div>
-        </div> */}
+            <p>&copy; 2024 Your Name</p>
+        </div>
     </footer>
   );
 };
